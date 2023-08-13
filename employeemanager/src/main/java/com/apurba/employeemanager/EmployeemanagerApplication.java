7c5234911f94652773f72b3ebe412f2cb0f06408
Apurba Debnath <apurba871@gmail.com>
2023-08-13 11:09:38 +0530

    Add backend resources

---
+++ employeemanager/src/main/java/com/apurba/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.apurba.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
